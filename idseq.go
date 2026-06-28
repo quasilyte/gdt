9e8b8e803c3cf727b517b4d04c9fbc3b6f9abc57
Iskander Sharipov <quasilyte@gmail.com>
2026-06-28 16:00:28 +0400

    make idseq start with 1 after an overflow

--- idseq.go
+++ idseq.go
@@ -5,7 +5,12 @@ type IDSeq[T integer] struct {
 }
 
 func (g *IDSeq[T]) Next() T {
+	old := g.Current
 	g.Current++
+	if g.Current < old {
+		// Overflow happened, should start with 1.
+		g.Current = 1
+	}
 	id := g.Current
 	return id
 }
